95be252d350e339cf8c79b92ce1f69e5a4497546
bogdan770 <bogdin666@gmail.com>
2021-10-24 15:30:25 +0300

    after review

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,42 @@
-let editButton = document.querySelector(".profile__edit-button");
-let displayPopup = document.querySelector(".popup_display");
-let closeButton = document.querySelector(".popup__close");
-let formElement = document.querySelector(".popup_box");
-let saveButton = document.querySelector(".popup__button");
-
-
 let userName = document.querySelector(".profile__username");
 let userProf = document.querySelector(".profile__userprof");
+//choosing strings from HTML
+
+console.log(userName.textContent, userProf.textContent);
+//checking that strings contain correct "parameter"
+
+let editButton = document.querySelector(".profile__edit-button");
+let displayPopup = document.querySelector(".popup");
+let closeButton = document.querySelector(".popup__close");
+let formElement = document.querySelector(".popup__form"); 
 
 
-let nameInput = document.querySelector(".popup__username");
-let jobInput = document.querySelector(".popup__prof")
+let nameInput = document.querySelector("#userName");
+let jobInput = document.querySelector("#userJob");
+console.log(nameInput, jobInput)
+// checking what JS in this values
 
+editButton.addEventListener('click', () =>{ 
+    nameInput.value = userName.textContent; 
+    jobInput.value = userProf.textContent; 
+    displayPopup.classList.add("popup__display");
+    // adding new class to the popup
+}); 
+// editButton get 'click' from the user, and turn the popup form with context from the page to him.
 
-editButton.addEventListener('click', () =>{
-    nameInput.value = userName.textContent;
-    jobInput.value = userProf.textContent;
-    displayPopup.setAttribute("style", "display: block");
-});
+formElement.addEventListener('submit',handleProfileFormSubmit);
 
-saveButton.addEventListener('click',() =>{
+function closePopup(){
+    displayPopup.classList.remove("popup__display");
+}
+//function that remove the class from variable displayPopup, it's mean than popup is disappear
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault()
     userName.textContent = nameInput.value;
     userProf.textContent = jobInput.value;
     closePopup()
-});
-
-function closePopup(){
-    displayPopup.setAttribute("style", "display: none");
 }
+//this function let us to change
 
 closeButton.addEventListener('click', closePopup);
