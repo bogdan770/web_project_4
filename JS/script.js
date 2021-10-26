51b10d59ee5c8dced64172ab1cb0361d9bbfce0b
bogdan770 <bogdin666@gmail.com>
2021-10-26 10:36:55 +0300

    done

--- JS/script.js
+++ JS/script.js
@@ -2,24 +2,21 @@ let userName = document.querySelector(".profile__username");
 let userProf = document.querySelector(".profile__userprof");
 //choosing strings from HTML
 
-console.log(userName.textContent, userProf.textContent);
-//checking that strings contain correct "parameter"
 
 let editButton = document.querySelector(".profile__edit-button");
 let displayPopup = document.querySelector(".popup");
 let closeButton = document.querySelector(".popup__close");
-let formElement = document.querySelector(".popup__form"); 
+let formElement = document.querySelector("#popup__form"); 
 
 
 let nameInput = document.querySelector("#userName");
 let jobInput = document.querySelector("#userJob");
-console.log(nameInput, jobInput)
-// checking what JS in this values
+
 
 editButton.addEventListener('click', () =>{ 
     nameInput.value = userName.textContent; 
     jobInput.value = userProf.textContent; 
-    displayPopup.classList.add("popup__display");
+    displayPopup.classList.add("popup_display");
     // adding new class to the popup
 }); 
 // editButton get 'click' from the user, and turn the popup form with context from the page to him.
@@ -27,7 +24,7 @@ editButton.addEventListener('click', () =>{
 formElement.addEventListener('submit',handleProfileFormSubmit);
 
 function closePopup(){
-    displayPopup.classList.remove("popup__display");
+    displayPopup.classList.remove("popup_display");
 }
 //function that remove the class from variable displayPopup, it's mean than popup is disappear
 
